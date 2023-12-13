264b1f42cc845c37172a7e16dd84b3a546e8c809
MakarNefedov2003 <152790224+MakarNefedov2003@users.noreply.github.com>
2023-12-13 17:51:57 +0300

    Update kata8.js

--- kata8.js
+++ kata8.js
@@ -1,3 +1,13 @@
 function multiply(a, b){
  return a * b
 }
+
+
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+function evenOrOdd(number) {
+  
+  if (number % 2 ===0) {
+    return "Even"
+  }
+ else {return "Odd"}
+}
